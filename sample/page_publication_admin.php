5d2d19e22a31e4a7eecb88e5d772d9f73e0d1b36
Takuto Yanagida <yanagida@stxst.com>
2021-07-19 15:19:16 +0900

    Minor change

--- sample/page_publication_admin.php
+++ sample/page_publication_admin.php
@@ -10,7 +10,7 @@
 
 
 function setup_template_admin() {
-	\wplug\bimeson_list\add_meta_box( __( 'Publications' ), 'page' );
+	\wplug\bimeson_list\add_meta_box( __( 'Publication List' ), 'page' );
 
 	add_action( 'save_post_page', function ( $post_id ) {
 		if ( ! current_user_can( 'edit_post', $post_id ) ) return;
