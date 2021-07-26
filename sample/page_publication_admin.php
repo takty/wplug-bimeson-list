cde2ed60ca75c540b1fdf10580da9f287d76ea3a
Takuto Yanagida <yanagida@stxst.com>
2021-07-27 08:43:53 +0900

    Minor fix

--- sample/page_publication_admin.php
+++ sample/page_publication_admin.php
@@ -4,13 +4,13 @@
  * Admin for the Template for Publication List Static Pages
  *
  * @author Takuto Yanagida
- * @version 2021-07-26
+ * @version 2021-07-27
  *
  */
 
 
 function setup_template_admin() {
-	\wplug\bimeson_list\add_meta_box( __( 'Publication list' ), 'page' );
+	\wplug\bimeson_list\add_meta_box( __( 'Publication List' ), 'page' );
 
 	add_action( 'save_post_page', function ( $post_id ) {
 		if ( ! current_user_can( 'edit_post', $post_id ) ) return;
